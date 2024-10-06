27096f14af003193081a5e4f91ef1c38be48f855
Grace Alea Chandra <gcha0035@student.monash.edu>
2024-10-06 22:17:56 +1100

    Added stacked area chart to html

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,6 @@
 var vg_1 = "australia_wineregions_map.vg.json";
 vegaEmbed("#map_chart", vg_1).then(function(result) {
 }).catch(console.error);
-var vg_2 = "region_production.vg.json";
-vegaEmbed("#bar_chart", vg_2).then(function(result) {
+var vg_2 = "production.vg.json";
+vegaEmbed("#area_chart", vg_2).then(function(result) {
 }).catch(console.error);
