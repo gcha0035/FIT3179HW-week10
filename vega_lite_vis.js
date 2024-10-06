055b14424d5625d5ceb796b2813d0eabd86b77e2
Grace Alea Chandra <gcha0035@student.monash.edu>
2024-10-06 21:46:19 +1100

    bar + line chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,6 @@
 var vg_1 = "australia_wineregions_map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "region_production.vg.json";
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
 }).catch(console.error);
